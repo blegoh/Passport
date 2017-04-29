5ad18484cfb13190016b30dcce13bb950183ad15
blegoh <yusufblegoh@gmail.com>
2017-04-29 10:18:20 +0700

    add accesor

--- app/Models/Lembur.php
+++ app/Models/Lembur.php
@@ -10,8 +10,20 @@ class Lembur extends Model
 
     protected $fillable = ['user_id','tanggal_lembur','jam','alasan','fee'];
 
+    protected $appends = ['name', 'jabatan'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getNameAttribute()
+    {
+        return $this->user->name;
+    }
+
+    public function getJabatanAttribute()
+    {
+        return $this->user->jabatan;
     }
 }
