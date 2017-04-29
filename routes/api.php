140422eb5d7723d18282c9ed9aad99bbb96daa75
blegoh <yusufblegoh@gmail.com>
2017-04-29 13:44:52 +0700

    add comment

--- routes/api.php
+++ routes/api.php
@@ -17,14 +17,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/user/lembur','API\LemburController@userLembur');
+Route::get('/user/lembur','API\LemburController@userLembur'); // njupuk lembur yg dilakukan si user
 
 Route::post('/register','API\RegisterController@store');
 
 
 Route::post('/lembur','API\LemburController@store');
 
-Route::get('/lembur','API\LemburController@index');
-Route::get('/lembur/all','API\LemburController@all');
+Route::get('/lembur','API\LemburController@index'); //iki admin ambil yg status masih 0
+Route::get('/lembur/all','API\LemburController@all'); // iki admin njupuk kabeh
 Route::get('/lembur/{id}/accept','API\LemburController@accept');
 Route::get('/lembur/{id}/reject','API\LemburController@reject');
